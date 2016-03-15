63be18a1358726cf28d3c007cdf99036ab44fc59
Maciej Lew <maciej.lew.1987@gmail.com>
2016-03-15 22:48:02 +0100

    Refactores dose model. Moves some common checks to private methods

--- app/scripts/Dose.js
+++ app/scripts/Dose.js
@@ -1,30 +1,32 @@
 function Dose() {
     var _min = null;
     var _max = null;
+    var checkType = function(value) {
+        if (!(value instanceof Number) && !(typeof value === 'number')) {
+            throw new TypeError('Dose value must be numeric!');
+        }
+    };
+    var checkRange = function(value) {
+        if (value < 0) {
+            throw new RangeError('Dose value can not be negative!');
+        }
+    }
     this.setMin = function(min) {
+        checkType(min);
+        checkRange(min);
         if (min instanceof Number) {
             _min = min;
         } else if (typeof min === 'number') {
             _min = new Number(min);
-        } else {
-            throw new TypeError('Dose value must be numeric!');
-        }
-        if (_min < 0) {
-            _min = null;
-            throw new RangeError('Dose value can not be negative!');
         }
     };
     this.setMax = function(max) {
+        checkType(max);
+        checkRange(max);
         if (max instanceof Number) {
             _max = max;
         } else if (typeof max === 'number') {
             _max = new Number(max);
-        } else {
-            throw new TypeError('Dose value must be numeric!');
-        }
-        if (_max < 0) {
-            _max = null;
-            throw new RangeError('Dose value can not be negative!');
         }
     };
     this.getMin = function() {
