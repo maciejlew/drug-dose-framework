8a99a191a1a7339547c9818fb60109561f862753
Maciej Lew <maciej.lew.1987@gmail.com>
2016-03-16 19:13:35 +0100

    Adds Dose Simple Parameters model

---
+++ app/scripts/DoseSimpleParameters.js
@@ -0,0 +1,56 @@
+function DoseSimpleParameters() {
+    var _a_min = null;
+    var _a_max = null;
+    var _b = null;
+    var checkType = function(value) {
+        if (!(value instanceof Number) && !(typeof value === 'number')) {
+            throw new TypeError('Dose parameter value must be numeric!');
+        }
+    };
+    var checkAMinSmallerThanAMax = function(a_min) {
+        if (_a_max !== null && a_min > _a_max) {
+            throw new RangeError('Dose parameter a_min can not be greater than a_max parameter!');
+        }
+    };
+    var checkAMaxGreaterThanAMin = function(a_max) {
+        if (_a_min !== null && a_max < _a_min) {
+            throw new RangeError('Dose parameter a_max can not be smaller than a_min parameter!');
+        }
+    };
+    this.setAMin = function(a_min) {
+        checkType(a_min);
+        checkAMinSmallerThanAMax(a_min);
+        if (a_min instanceof Number) {
+            _a_min = a_min;
+        } else if (typeof a_min === 'number') {
+            _a_min = new Number(a_min);
+        }
+    };
+    this.setAMax = function(a_max) {
+        checkType(a_max);
+        checkAMaxGreaterThanAMin(a_max);
+        if (a_max instanceof Number) {
+            _a_max = a_max;
+        } else if (typeof a_max === 'number') {
+            _a_max = new Number(a_max);
+        }
+    };
+    this.setB = function(b) {
+        checkType(b);
+        if (b instanceof Number) {
+            _b = b;
+        } else if (typeof b === 'number') {
+            _b = new Number(b);
+        }
+    };
+    this.getAMin = function() {
+        return _a_min;
+    };
+    this.getAMax = function() {
+        return _a_max;
+    };
+    this.getB = function() {
+        return _b;
+    };
+}
+DoseSimpleParameters.prototype = new DoseSimpleParameters();
---
+++ test/spec/DoseSimpleParametersSpec.js
@@ -0,0 +1,94 @@
+'use strict';
+
+describe('Dose Simple Parameters', function() {
+
+    it('should set "a_min" dose when "a_min" is a number', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        expect(dose_parameters.getAMin()).toBeNull();
+        dose_parameters.setAMin(12);
+        expect(dose_parameters.getAMin()).toEqual(12);
+        dose_parameters.setAMin(new Number(13));
+        expect(dose_parameters.getAMin()).toEqual(13);
+        dose_parameters.setAMin(11.11);
+        expect(dose_parameters.getAMin()).toEqual(11.11);
+    });
+
+    it('should not set "a_min" when "a_min" is not a number', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        var expected_exception = new TypeError('Dose parameter value must be numeric!');
+        expect(dose_parameters.getAMin()).toBeNull();
+        expect(function() {dose_parameters.setAMin('string');}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMin(true);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMin(false);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMin([]);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMin({});}).toThrow(expected_exception);
+        expect(dose_parameters.getAMin()).toBeNull();
+    });
+    
+    it('should set "a_max" dose when "a_max" is a number', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        expect(dose_parameters.getAMax()).toBeNull();
+        dose_parameters.setAMax(12);
+        expect(dose_parameters.getAMax()).toEqual(12);
+        dose_parameters.setAMax(new Number(13));
+        expect(dose_parameters.getAMax()).toEqual(13);
+        dose_parameters.setAMax(11.11);
+        expect(dose_parameters.getAMax()).toEqual(11.11);
+    });
+
+    it('should not set "a_max" when "a_max" is not a number', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        var expected_exception = new TypeError('Dose parameter value must be numeric!');
+        expect(dose_parameters.getAMax()).toBeNull();
+        expect(function() {dose_parameters.setAMax('string');}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMax(true);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMax(false);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMax([]);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setAMax({});}).toThrow(expected_exception);
+        expect(dose_parameters.getAMax()).toBeNull();
+    });
+
+    it('should set "b" dose when "b" is a number', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        expect(dose_parameters.getB()).toBeNull();
+        dose_parameters.setB(12);
+        expect(dose_parameters.getB()).toEqual(12);
+        dose_parameters.setB(new Number(13));
+        expect(dose_parameters.getB()).toEqual(13);
+        dose_parameters.setB(11.11);
+        expect(dose_parameters.getB()).toEqual(11.11);
+    });
+
+    it('should not set "b" when "b" is not a number', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        var expected_exception = new TypeError('Dose parameter value must be numeric!');
+        expect(dose_parameters.getB()).toBeNull();
+        expect(function() {dose_parameters.setB('string');}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setB(true);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setB(false);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setB([]);}).toThrow(expected_exception);
+        expect(function() {dose_parameters.setB({});}).toThrow(expected_exception);
+        expect(dose_parameters.getB()).toBeNull();
+    });
+    
+    it('should not set "a_min" when "a_min" is greater than "a_max"', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        var expected_exception = new RangeError('Dose parameter a_min can not be greater than a_max parameter!');
+        dose_parameters.setAMax(10);
+        expect(dose_parameters.getAMax()).toEqual(10);
+        expect(dose_parameters.getAMin()).toBeNull();
+        expect(function() {dose_parameters.setAMin(11);}).toThrow(expected_exception);
+        expect(dose_parameters.getAMin()).toBeNull();
+    });
+    
+    it('should not set "a_max" when "a_max" is smaller than "a_min"', function() {
+        var dose_parameters = new DoseSimpleParameters();
+        var expected_exception = new RangeError('Dose parameter a_max can not be smaller than a_min parameter!');
+        dose_parameters.setAMin(10);
+        expect(dose_parameters.getAMin()).toEqual(10);
+        expect(dose_parameters.getAMax()).toBeNull();
+        expect(function() {dose_parameters.setAMax(9);}).toThrow(expected_exception);
+        expect(dose_parameters.getAMax()).toBeNull();
+    });
+
+});
