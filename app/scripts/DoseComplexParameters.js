f86748a97f715948a9fdd8f0812ac234ca686ce7
Maciej Lew <maciej.lew.1987@gmail.com>
2016-03-16 21:04:47 +0100

    Adds Dose Complex Parameters model

---
+++ app/scripts/DoseComplexParameters.js
@@ -0,0 +1,64 @@
+function DoseComplexParameters() {
+    var _ranges = [];
+    var _doses = [];
+    var checkType = function(value) {
+        if (!(value instanceof Number) && !(typeof value === 'number')) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+    var checkRangeType = function(range) {
+        if (checkType(range) === false) {
+            throw new TypeError('Range parameter value must be numeric!');
+        }
+    };
+    var checkDoseType = function(range) {
+        if (checkType(range) === false) {
+            throw new TypeError('Dose parameter value must be numeric!');
+        }
+    };
+    var checkRangesAmount = function(ranges) {
+        if (_doses.length !== 0 && ranges.length !== _doses.length) {
+            throw new RangeError('Dose parameters amount and ranges parameters differs!');
+        }
+    };
+    var checkDosesAmount = function(doses) {
+        if (_ranges.length !== 0 && doses.length !== _ranges.length) {
+            throw new RangeError('Dose parameters amount and ranges parameters differs!');
+        }
+    };
+    this.setRanges = function(ranges) {
+        checkRangesAmount(ranges);
+        var tmp = [];
+        for (var i = 0 ; i < ranges.length ; i++) {
+            checkRangeType(ranges[i]);
+            if (ranges[i] instanceof Number) {
+                tmp.push(ranges[i]);
+            } else {
+                tmp.push(new Number(ranges[i]));
+            }
+        }
+        _ranges = tmp;
+    };
+    this.setDoses = function(doses) {
+        checkDosesAmount(doses);
+        var tmp = [];
+        for (var i = 0 ; i < doses.length ; i++) {
+            checkDoseType(doses[i]);
+            if (doses[i] instanceof Number) {
+                tmp.push(doses[i]);
+            } else {
+                tmp.push(new Number(doses[i]));
+            }
+        }
+        _doses = tmp;
+    };
+    this.getRanges = function() {
+        return _ranges;
+    };
+    this.getDoses = function() {
+        return _doses;
+    };
+}
+DoseComplexParameters.prototype = new DoseComplexParameters();
---
+++ test/spec/DoseComplexParametersSpec.js
@@ -0,0 +1,141 @@
+'use strict';
+
+describe('Dose Complex Parameters', function() {
+
+    it('should set ranges when all ranges are integer numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var integer_ranges = [1, 2, 3];
+        var number_ranges = [new Number(1), new Number(2), new Number(3)];
+        expect(dose_parameters.getRanges()).toEqual([]);
+        dose_parameters.setRanges(integer_ranges);
+        expect(dose_parameters.getRanges()).toEqual(number_ranges);
+    });
+    
+    it('should set ranges when all ranges are float numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var float_ranges = [1.0, 2.0, 3.0];
+        var number_ranges = [new Number(1), new Number(2), new Number(3)];
+        expect(dose_parameters.getRanges()).toEqual([]);
+        dose_parameters.setRanges(float_ranges);
+        expect(dose_parameters.getRanges()).toEqual(number_ranges);
+    });
+    
+    it('should set ranges when all ranges are numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var number_ranges = [new Number(1), new Number(2), new Number(3)];
+        expect(dose_parameters.getRanges()).toEqual([]);
+        dose_parameters.setRanges(number_ranges);
+        expect(dose_parameters.getRanges()).toEqual(number_ranges);
+    });
+    
+    it('should set ranges when ranges are mixed numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var number_ranges = [new Number(1), new Number(2), new Number(3)];
+        var mixed_ranges = [1, 2.0, new Number(3)];
+        expect(dose_parameters.getRanges()).toEqual([]);
+        dose_parameters.setRanges(mixed_ranges);
+        expect(dose_parameters.getRanges()).toEqual(number_ranges);
+    });
+    
+    it('should not set ranges when at least one range is not a number', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var invalid_ranges = [new Number(1), new Number(2), "3"];
+        var expected_exception = new TypeError('Range parameter value must be numeric!');
+        expect(dose_parameters.getRanges()).toEqual([]);
+        expect(function() {dose_parameters.setRanges(invalid_ranges);}).toThrow(expected_exception);
+        expect(dose_parameters.getRanges()).toEqual([]);
+    });
+    
+    it('should set doses when all doses are integer numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var integer_doses = [1, 2, 3];
+        var number_doses = [new Number(1), new Number(2), new Number(3)];
+        expect(dose_parameters.getDoses()).toEqual([]);
+        dose_parameters.setDoses(integer_doses);
+        expect(dose_parameters.getDoses()).toEqual(number_doses);
+    });
+    
+    it('should set doses when all doses are float numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var float_doses = [1.0, 2.0, 3.0];
+        var number_doses = [new Number(1), new Number(2), new Number(3)];
+        expect(dose_parameters.getDoses()).toEqual([]);
+        dose_parameters.setDoses(float_doses);
+        expect(dose_parameters.getDoses()).toEqual(number_doses);
+    });
+    
+    it('should set doses when all doses are numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var number_doses = [new Number(1), new Number(2), new Number(3)];
+        expect(dose_parameters.getDoses()).toEqual([]);
+        dose_parameters.setDoses(number_doses);
+        expect(dose_parameters.getDoses()).toEqual(number_doses);
+    });
+    
+    it('should set doses when doses are mixed numbers', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var number_doses = [new Number(1), new Number(2), new Number(3)];
+        var mixed_doses = [1, 2.0, new Number(3)];
+        expect(dose_parameters.getDoses()).toEqual([]);
+        dose_parameters.setDoses(mixed_doses);
+        expect(dose_parameters.getDoses()).toEqual(number_doses);
+    });
+    
+    it('should not set doses when at least one range is not a number', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var invalid_doses = [new Number(1), new Number(2), "3"];
+        var expected_exception = new TypeError('Dose parameter value must be numeric!');
+        expect(dose_parameters.getDoses()).toEqual([]);
+        expect(function() {dose_parameters.setDoses(invalid_doses);}).toThrow(expected_exception);
+        expect(dose_parameters.getDoses()).toEqual([]);
+    });
+    
+    it('should not set doses when doses amount and ranges amount differs', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var ranges  = [new Number(1), new Number(2), new Number(3)];
+        var doses_deficit = [new Number(1), new Number(2)];
+        var doses_surplus = [new Number(1), new Number(2), new Number(3), new Number(4)];
+        var expected_exception = new RangeError('Dose parameters amount and ranges parameters differs!');
+        expect(dose_parameters.getDoses()).toEqual([]);
+        expect(dose_parameters.getRanges()).toEqual([]);
+        dose_parameters.setRanges(ranges);
+        expect(dose_parameters.getRanges()).toEqual(ranges);
+        expect(function() {dose_parameters.setDoses(doses_deficit);}).toThrow(expected_exception);
+        expect(dose_parameters.getDoses()).toEqual([]);
+        expect(function() {dose_parameters.setDoses(doses_surplus);}).toThrow(expected_exception);
+        expect(dose_parameters.getDoses()).toEqual([]);
+    });
+    
+    it('should not set doses when doses amount and ranges amount differs', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var ranges  = [new Number(1), new Number(2), new Number(3)];
+        var doses_deficit = [new Number(1), new Number(2)];
+        var doses_surplus = [new Number(1), new Number(2), new Number(3), new Number(4)];
+        var expected_exception = new RangeError('Dose parameters amount and ranges parameters differs!');
+        expect(dose_parameters.getDoses()).toEqual([]);
+        expect(dose_parameters.getRanges()).toEqual([]);
+        dose_parameters.setRanges(ranges);
+        expect(dose_parameters.getRanges()).toEqual(ranges);
+        expect(function() {dose_parameters.setDoses(doses_deficit);}).toThrow(expected_exception);
+        expect(dose_parameters.getDoses()).toEqual([]);
+        expect(function() {dose_parameters.setDoses(doses_surplus);}).toThrow(expected_exception);
+        expect(dose_parameters.getDoses()).toEqual([]);
+    });
+    
+    it('should not set ranges when doses amount and ranges amount differs', function() {
+        var dose_parameters = new DoseComplexParameters();
+        var doses  = [new Number(1), new Number(2), new Number(3)];
+        var ranges_deficit = [new Number(1), new Number(2)];
+        var ranges_surplus = [new Number(1), new Number(2), new Number(3), new Number(4)];
+        var expected_exception = new RangeError('Dose parameters amount and ranges parameters differs!');
+        expect(dose_parameters.getDoses()).toEqual([]);
+        expect(dose_parameters.getRanges()).toEqual([]);
+        dose_parameters.setDoses(doses);
+        expect(dose_parameters.getDoses()).toEqual(doses);
+        expect(function() {dose_parameters.setRanges(ranges_deficit);}).toThrow(expected_exception);
+        expect(dose_parameters.getRanges()).toEqual([]);
+        expect(function() {dose_parameters.setRanges(ranges_surplus);}).toThrow(expected_exception);
+        expect(dose_parameters.getRanges()).toEqual([]);
+    });
+
+});
