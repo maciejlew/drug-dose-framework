2f66f2b8da1434a68483512cb094e52e45aec346
Maciej Lew <maciej.lew.1987@gmail.com>
2016-03-22 21:37:43 +0100

    Adds Dose Strategy

---
+++ app/scripts/DoseStrategy.js
@@ -0,0 +1,24 @@
+function DoseStrategy() {
+    var _dose_parameters = null;
+    this.checkWeightType = function(weight) {
+        if (!(typeof weight === 'number') && !(weight instanceof Number)) {
+            throw new TypeError('Weight must be number!');
+        } else if (weight < 0) {
+            throw new RangeError('Weight must be positive number!');
+        }
+    };
+    this.checkDoseParameters = function(dose_parameters) {
+        throw new Error("Abstract method!");
+    };
+    this.setDoseParameters = function (dose_parameters) {
+        this.checkDoseParameters(dose_parameters);
+        _dose_parameters = dose_parameters;
+    };
+    this.getDoseParameters = function () {
+        return _dose_parameters;
+    };
+    this.calculateDose = function(weight) {
+        throw new Error("Abstract method!");
+    };
+}
+DoseStrategy.prototype = new DoseStrategy();
---
+++ test/spec/DoseStrategySpec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('Dose Strategy', function() {
+
+    it('should throw an exception when setting parameters', function() {
+        var dose_strategy = new DoseStrategy();
+        var dose_parameters = new DoseSimpleParameters();
+        var expected_exception = new Error("Abstract method!");
+        expect(function() {dose_strategy.setDoseParameters(dose_parameters);}).toThrow(expected_exception);
+    });
+    
+    it('should throw an exception when calculating dose', function() {
+        var dose_strategy = new DoseStrategy();
+        var expected_exception = new Error("Abstract method!");
+        expect(function() {dose_strategy.calculateDose(new Number(10));}).toThrow(expected_exception);
+    });
+    
+    it('should throw an exception when parsing non number weight', function() {
+        var dose_strategy = new DoseStrategy();
+        var expected_exception = new TypeError('Weight must be number!');
+        expect(function() {dose_strategy.checkWeightType(null);}).toThrow(expected_exception);
+        expect(function() {dose_strategy.checkWeightType('string');}).toThrow(expected_exception);
+        expect(function() {dose_strategy.checkWeightType([]);}).toThrow(expected_exception);
+        expect(function() {dose_strategy.checkWeightType({});}).toThrow(expected_exception);
+    });
+    
+    it('should not throw an exception when parsing positive number weight', function() {
+        var dose_strategy = new DoseStrategy();
+        dose_strategy.checkWeightType(1);
+        dose_strategy.checkWeightType(1.0);
+        dose_strategy.checkWeightType(new Number(1));
+    });
+    
+    it('should throw an exception when parsing negative number weight', function() {
+        var dose_strategy = new DoseStrategy();
+        var expected_exception = new RangeError('Weight must be positive number!');
+        expect(function() {dose_strategy.checkWeightType(-1);}).toThrow(expected_exception);
+        expect(function() {dose_strategy.checkWeightType(-1.0);}).toThrow(expected_exception);
+        expect(function() {dose_strategy.checkWeightType(new Number(-1));}).toThrow(expected_exception);
+    });
+    
+});
