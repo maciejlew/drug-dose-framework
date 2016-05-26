7f5e97e23beedaa3039392a1863bd372e8e20f67
Maciej Lew <maciej.lew.1987@gmail.com>
2016-05-26 10:23:51 +0200

    Adds translations for each view title attribute

--- app/scripts/translations.js
+++ app/scripts/translations.js
@@ -1,6 +1,6 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('de_DE', {"Type drug name":"Schreiben Sie die Medikamentennamen","calculate":"berechnen","calculate dose":"berechnen Dosis","close":"schließen","description":"Beschreibung","dose":"Dosis","drug":"Arzneimittel","exception occured":"Ausnahme ist aufgetreten","show details":"Details anzeigen","type":"Typ","weight":"Gewicht"});
-    gettextCatalog.setStrings('pl_PL', {"Type drug name":"Wpisz nazwę leku","calculate":"oblicz","calculate dose":"oblicz dawkę","close":"zamknij","description":"opis","dose":"dawka","drug":"lek","exception occured":"wystąpił wyjątek","show details":"pokaż szczegóły","type":"typ","weight":"waga"});
+    gettextCatalog.setStrings('de_DE', {"Drug Details":"Arzneimittel Details","Drug Dose":"Arzneimittel Dosis","Drug List":"Arzneimittel Liste","Language settings":"Spracheneinstellungen","Type drug name":"Schreiben Sie die Medikamentennamen","calculate":"berechnen","close":"schließen","description":"Beschreibung","dose":"Dosis","drug":"Arzneimittel","exception occured":"Ausnahme ist aufgetreten","type":"Typ","weight":"Gewicht"});
+    gettextCatalog.setStrings('pl_PL', {"Drug Details":"Opis leku","Drug Dose":"Dawkowanie leku","Drug List":"Lista leków","Language settings":"Ustawienia językowe","Type drug name":"Wpisz nazwę leku","calculate":"oblicz","close":"zamknij","description":"opis","dose":"dawka","drug":"lek","exception occured":"wystąpił wyjątek","type":"typ","weight":"waga"});
 /* jshint +W100 */
 }]);
--- doc/changelog.md
+++ doc/changelog.md
@@ -3,6 +3,9 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Added
+- translations for each view title attribute
+
 ### Changed
 - option buttons to regular buttons at drug list view
 
